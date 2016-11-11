e851a9032a7589ec1dbcbd30e7f8aeabf5a944c9
John Bafford <john@bafford.com>
2016-11-10 22:16:20 -0500

    Use newer composer event handler typehint

--- Composer/WebLibInstall.php
+++ Composer/WebLibInstall.php
@@ -4,11 +4,11 @@ namespace Bafford\WebLibBundle\Composer;
 
 use Symfony\Component\Process\Process;
 
-use Composer\Script\CommandEvent;
+use Composer\Script\Event;
 
 class WebLibInstall extends \Sensio\Bundle\DistributionBundle\Composer\ScriptHandler
 {
-    public static function installComponents(CommandEvent $event)
+    public static function installComponents(Event $event)
     {
         $options = self::getOptions($event);
         
@@ -20,12 +20,12 @@ class WebLibInstall extends \Sensio\Bundle\DistributionBundle\Composer\ScriptHan
         static::executeCommand($event, $consoleDir, 'bafford:weblib:install');
     }
     
-    public static function postInstall(CommandEvent $event)
+    public static function postInstall(Event $event)
     {
         return self::installComponents($event);
     }
     
-    public static function postUpdate(CommandEvent $event)
+    public static function postUpdate(Event $event)
     {
         return self::installComponents($event);
     }
